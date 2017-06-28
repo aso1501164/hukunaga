b113e64adf016d1ae6f7a1c5ab38bc2ca94ec0f0
aso1501133 <1501133@st.asojuku.ac.jp>
2017-06-28 10:09:36 +0900

    k

--- src/model/History.java
+++ src/model/History.java
@@ -9,6 +9,20 @@ public class History  implements Serializable{
 	private String student_id = "";
 	private String subject_id_1 = "";
 	private String subject_id_2 = "";
+	private String subject_name_1;
+	private String subject_name_2;
+	public String getSubject_name_1() {
+		return subject_name_1;
+	}
+	public void setSubject_name_1(String subject_name_1) {
+		this.subject_name_1 = subject_name_1;
+	}
+	public String getSubject_name_2() {
+		return subject_name_2;
+	}
+	public void setSubject_name_2(String subject_name_2) {
+		this.subject_name_2 = subject_name_2;
+	}
 	private String year = "";
 	public String getStudent_id() {
 		return student_id;
